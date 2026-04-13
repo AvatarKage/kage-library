c8f8ea42b9dd3cffbf4d5b4abd0de16f763e0bea
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-04-13 02:51:13 -0400

    added sandbox script

---
+++ sandbox.ts
@@ -0,0 +1,65 @@
+import "./typescript/extensions/string.js"
+import "./typescript/extensions/object.js"
+
+import Snowflake from "./typescript/classes/snowflake.js";
+import { config } from "./typescript/modules/config/readConfig.js";
+import formatNumber from "./typescript/helpers/formatNumber.js";
+import URL from "./typescript/classes/url.js";
+import { toMs } from "./typescript/helpers/misc.js";
+import Database from "./typescript/classes/database.js";
+import Identifier from "./typescript/classes/identifier.js";
+import { log } from "./typescript/modules/logging/log.js";
+import toArray from "./typescript/helpers/toArray.js";
+import Metadata from "./typescript/classes/metadata.js";
+import parseDuration from "./typescript/helpers/parseDuration.js";
+
+export const snowflake = new Snowflake(config.generation.epoch);
+
+log.snowflake.info(snowflake.decode("36566882404270080"));
+log.number.info(toArray(formatNumber(8946518965)));
+
+const url = new URL("http://www.example.org/test?q=78541#top");
+url.updateQuery("q", "confirmed");
+url.updateQuery("a", "bro");
+url.updateHash("cc");
+url.updateDomain("example.com");
+url.updateSubdomain("");
+url.updatePath("premium");
+url.updateProtocol("https");
+
+log.url.info(url);
+
+log.number.info(toMs(1));
+
+export const db = {
+    audits: new Database("data/audits.sqlite"),
+    contributors: new Database("data/contributors.sqlite"),
+    metadata: new Database("data/metadata.sqlite")
+};
+
+// Populate databases on first run
+db.audits.transaction((query) => {
+    if (!query("SELECT * FROM identifiers LIMIT 1").success) { query("./sql/identifiers.sql"); };
+});
+
+db.contributors.transaction((query) => {
+    if (!query("SELECT * FROM contributors LIMIT 1").success) { query("./sql/contributors.sql"); };
+});
+
+db.metadata.transaction((query) => {
+    if (!query("SELECT * FROM metadata LIMIT 1").success) { query("./sql/metadata.sql"); };
+});
+
+log.db.debug(db.contributors.query("SELECT * FROM contributors"));
+
+export const id = new Identifier(db.audits);
+const shortlink = id.generate("SHORTLINK");
+
+log.id.success(shortlink);
+log.id.info(id.get(shortlink));
+
+const md = new Metadata(db.metadata);
+log.crawler.success(await md.get(url.href));
+
+// Better if in a cron
+log.crawler.success(md.clearCache(parseDuration("1d")));
